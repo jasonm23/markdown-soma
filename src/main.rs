055ac5b6a6ec11d9eee7f7246876e7ee4b08e024
jasonm23 <jasonm23@gmail.com>
2022-06-11 17:42:33 +0700

    Add on darkmode - testing

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,25 @@ fn main() {
     let matches = App::new("soma")
         .author(crate_authors!())
         .version(crate_version!())
-        .about("soma - socket driven live markdown preview server")
+        .about("soma - websocket driven live markdown preview server")
         .arg(
             Arg::with_name("host")
                 .long("host")
+                .value_name("address")
                 .help("The host address this server will listen on, Defaults to `localhost`.")
+                .takes_value(true)
         )
         .arg(
             Arg::with_name("port")
                 .long("port")
                 .help("The port number the server will listen on, Defaults to 0 (system assigned)")
+                .takes_value(true)
+        )
+        .arg(
+          Arg::with_name("darkmode")
+                .long("darkmode")
+                .short("D")
+                .help("Dark mode (defaults to Light mode)")
         )
         .get_matches();
 
@@ -27,6 +36,10 @@ fn main() {
         matches.value_of("host").unwrap_or("localhost"),
         matches.value_of("port").unwrap_or("0")
     )).unwrap();
+
+    if matches.value_of("darkmode").unwrap_or("").contains("darkmode")  {
+        server.set_custom_css(vec!["https://raw.githubusercontent.com/StylishThemes/GitHub-Dark/master/github-dark.user.css".to_string()]).unwrap();
+    }
 
     // TODO.
     // server.set_custom_css
