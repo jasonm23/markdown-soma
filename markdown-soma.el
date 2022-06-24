c576a3e9b5b2f17eebb6290e58853a59516e77cb
jasonm23 <jasonm23@gmail.com>
2022-06-24 13:28:32 +0700

    0.1.5

--- markdown-soma.el
+++ markdown-soma.el
@@ -5,7 +5,7 @@
 ;; Author: Jason Milkins <jasonm23@gmail.com>
 ;; URL: https://github.com/jasonm23/markdown-soma
 ;; Keywords: wp, docs, text, markdown
-;; Version: 0.1.4
+;; Version: 0.1.5
 ;; Package-Requires: ((emacs "25")  (s "1.11.0") (f "0.20.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
